65982257657992eb3229d5730a03b684f923320a
AbdelhakDouiba <douibaabdelhak2003@gmail.com>
2023-07-23 12:32:58 -0700

    Print string function()

---
+++ print_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ *print_string - Prints a string to the standard output.
+ *
+ *@str: A va_list containing the arguments
+ *needed for the function.
+ *
+ *Return: lenght of string.
+ **/
+
+int print_string(va_list str)
+{
+	int i = 0;
+	char *s = va_arg(str, char *);
+
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	return (i);
+}
