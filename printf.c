44f7b7073239deed2f945ebc51625ced1cb87190
Douibaabdelhak <Douibaabdelhak2003@gmail.com>
2023-07-24 10:06:29 -0700

    done

--- printf.c
+++ printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_printf - Custom implementation of
  *the printf function.
@@ -10,43 +11,13 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, j, holder_c = 0, count = 0;
+	int len_string;
 
-	func op[] = {
-	{"c", print_char},
-	{"i", print_int},
-	{"d", print_int},
-	{"s", print_string},
-	{"%", print_per},
-	{"R", rot13},
-	{"r", rev_string},
-	{NULL, NULL}};
 	va_start(args, format);
 	if ((!format || (format[0] == '%' && !format[1])) ||
 		(format[0] == '%' && format[1] == ' ' && !format[2]))
 		return (-1);
-	while (format[i] != '\0')
-	{
-		if (format[i] != '%')
-			_putchar(format[i]), holder_c++;
-		else
-		{
-			j = 0;
-			while (op[j].ch)
-			{
-				if (format[i + 1] == *op[j].ch)
-				{
-					count += (op[j].f)(args), i++;
-					break;
-				}
-				j++;
-			}
-			if (op[j].ch == NULL)
-				count += 1,_putchar('%');
-		}
-		i++;
-	}
-	holder_c += count;
+	len_string = check(format, args);
 	va_end(args);
-	return (holder_c);
-	}
+	return (len_string);
+}
